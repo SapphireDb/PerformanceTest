c26d48d1c7fa9a23f91723f8dc7fadce5aab336f
morrisjdev <morrisj@live.de>
2020-02-11 15:39:51 +0100

    added new localhost evaluation

--- PerformanceTestClient/consts.js
+++ PerformanceTestClient/consts.js
@@ -1,8 +1,8 @@
+// export const perfServerUrl = 'sapphire-perf-server.azurewebsites.net';
+// export const useSsl = true;
+// export const perfDataServerUrl = 'https://sapphire-perf-data-server.azurewebsites.net';
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.perfServerUrl = 'sapphire-perf-server.azurewebsites.net';
-exports.useSsl = true;
-exports.perfDataServerUrl = 'https://sapphire-perf-data-server.azurewebsites.net';
-// export const perfServerUrl = 'localhost:5000';
-// export const useSsl = false;
-// export const perfDataServerUrl = 'http://localhost:5003';
+exports.perfServerUrl = '192.168.0.67:5000';
+exports.useSsl = false;
+exports.perfDataServerUrl = 'http://192.168.0.67:5001';
 exports.entryCount = 1;
--- PerformanceTestClient/consts.ts
+++ PerformanceTestClient/consts.ts
@@ -1,9 +1,9 @@
-export const perfServerUrl = 'sapphire-perf-server.azurewebsites.net';
-export const useSsl = true;
-export const perfDataServerUrl = 'https://sapphire-perf-data-server.azurewebsites.net';
+// export const perfServerUrl = 'sapphire-perf-server.azurewebsites.net';
+// export const useSsl = true;
+// export const perfDataServerUrl = 'https://sapphire-perf-data-server.azurewebsites.net';
 
-// export const perfServerUrl = 'localhost:5000';
-// export const useSsl = false;
-// export const perfDataServerUrl = 'http://localhost:5003';
+export const perfServerUrl = '192.168.0.67:5000';
+export const useSsl = false;
+export const perfDataServerUrl = 'http://192.168.0.67:5001';
 
 export const entryCount = 1;
